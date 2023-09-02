0046b566d0ac4cb7ea52229d41834cdc6d6f431e
baihu433 <1522099983@qq.com>
2023-09-02 10:13:58 +0800

    测试提交

--- install.sh
+++ install.sh
@@ -51,6 +51,8 @@ echo -e ${cyan}白狐 Script 不会执行任何恶意命令${background}
 echo -e ${cyan}白狐 Script 不会执行任何恶意命令${background}
 echo -e ${cyan}白狐 Script 不会执行任何恶意命令${background}
 echo -e ${cyan}如果您同意安装 请输入 ${green}同意安装${background}
+echo -e ${cyan}注意：同意安装即同意本项目的用户协议${background}
+echo -e ${cyan}用户协议链接：https://gitee.com/baihu433/Yunzai-Bot-Shell/blob/master/manage/用户协议.txt${background}
 echo -e ${white}"=========================="${background}
 echo -en ${green}请输入您的选项: ${background};read yn
 if [  "yn" == "同意安装" ]
