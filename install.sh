2a0d10e48f09faf2d1e69d6c92020e9b5f09b337
baihu433 <1522099983@qq.com>
2023-09-24 11:08:33 +0800

    修复强制更新

--- install.sh
+++ install.sh
@@ -87,7 +87,7 @@ function Script_Install(){
     echo -e ${white}=========================${background}
     echo -e ${red}" "白狐 ${yellow}BOT ${green}Install ${cyan}Script ${background}
     echo -e "  "————"  "————"  "————"  "————"  "
-    echo -e ${green}" "版本:" "v0.2.2 ${cyan}\(20230917\) ${background}
+    echo -e ${green}" "版本:" "v0.2.6 ${cyan}\(20230924\) ${background}
     echo -e ${green}" "作者:" "${cyan}白狐"   "\(baihu433\) ${background}
     echo -e ${white}=========================${background}
     echo
