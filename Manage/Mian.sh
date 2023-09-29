5d7cf07b083f286c14a83580343d680a529b726c
baihu433 <1522099983@qq.com>
2023-09-29 17:56:14 +0800

    up

--- Manage/Mian.sh
+++ Manage/Mian.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
-export ver=0.3.1
+export ver=0.3.2
 cd $HOME
 export red="\033[31m"
 export green="\033[32m"
@@ -424,13 +424,6 @@ do
     i=$((${i}+1))
     sleep 0.05s
     echo -e ${i}
-    if [[ ${i} == 50 ]];then
-        if echo ${tmux_windows} | grep -q 'open terminal failed: not a terminal'
-        then
-            echo -e "${Tmux_Name}启动错误!! '\n'错误原因: open terminal failed: not a terminal"
-        fi
-        return 1
-    fi
     if [[ ${i} == 100 ]];then
         echo -e "错误: 启动失败"
         return 1
@@ -446,7 +439,13 @@ tmux_gauge ${Bot_Name}
 bot_tmux_attach(){
 if (${dialog_whiptail} --yesno "${Bot_Name} [已"${Start_Stop_Restart}"] \n是否打开${Bot_Name}窗口" 8 50)
 then
-    tmux attach -t ${Bot_Name}
+    if ! tmux attach -t ${Bot_Name};then
+        tmux_windows_attach=$(tmux attach -t ${Bot_Name})
+        if echo ${tmux_windows_attach} | grep -q 'open terminal failed: not a terminal'
+        then
+            ${dialog_whiptail} --yesno "${Bot_Name}启动错误 \n错误原因:open terminal failed: not a terminal" 8 50
+        fi
+    fi
 fi
 }
 
